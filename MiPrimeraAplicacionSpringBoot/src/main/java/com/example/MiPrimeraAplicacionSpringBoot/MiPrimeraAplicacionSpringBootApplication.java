7e8f0ca7ea61ea35a06fd4fda7f22fc49885a191
Abzael18 <vyeah1165@gmail.com>
2026-09-24 08:44:19 +0200

    Actualizacion para crear WAR

--- MiPrimeraAplicacionSpringBoot/src/main/java/com/example/MiPrimeraAplicacionSpringBoot/MiPrimeraAplicacionSpringBootApplication.java
+++ MiPrimeraAplicacionSpringBoot/src/main/java/com/example/MiPrimeraAplicacionSpringBoot/MiPrimeraAplicacionSpringBootApplication.java
@@ -2,12 +2,20 @@ package com.example.MiPrimeraAplicacionSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MiPrimeraAplicacionSpringBootApplication {
+public class MiPrimeraAplicacionSpringBootApplication
+        extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
+        return application.sources(MiPrimeraAplicacionSpringBootApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MiPrimeraAplicacionSpringBootApplication.class, args);
     }
-
 }
